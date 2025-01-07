bd3ab9bb1145796ef1a55028aaab815956781446
ayushman76 <avenuedesign768@gmail.com>
2025-01-07 18:58:32 +0530

    ss

---
+++ pranjal-space/js-method-practice/app.js
@@ -0,0 +1,27 @@
+// let array = [["Muskan","Ayushman", "Pranjal", "Jinat"], [1, 2, 3, 4, 5, 6, 7], ["UI Dev", "UI Dev", "UI Dev"]];
+
+// let array1 = [1, 2, 3, 4, 5, 6, 7];
+// let array2 = ["Apple", "Banana", "Grapes", "Mango"];
+
+// let array = ["a", "c", "e", "b" , "d"];
+// console.log(array.sort());
+
+// let array = ["Muskan","Ayushman", "Pranjal", "Jinat", "Lokesh"];
+// let newarray = array.toSpliced(0, 2);
+// array.pop();
+// let newarray = array.slice(0, 3);
+// let newarray = array.splice(0, 2);
+// console.log(newarray);
+
+// let newArray2 = array1.concat(array2);
+// console.log(newArray2);
+// array.push("Muskan");
+// array.unshift("Muskan");
+// array.unshift("Taslim");
+
+
+// array.shift();
+
+
+
+
