75e6887ee312edce62602535dd4e647c4bc1acf7
Tasli Khan <tasm8676@gmail.com>
2025-01-07 08:20:27 +0530

    g

--- ayushman-space/practice/event-list/app.js
+++ ayushman-space/practice/event-list/app.js
@@ -1,29 +1,5 @@
 // function clickme(x, y){
 //     console.log(x +" "+ y);
-<<<<<<< HEAD
-// }
-
-// let btn = document.getElementById('btn').addEventListener('click', function(){
-//     alert("Hello  I am Updated One..");
-// });
-
-// btn.addEventListener('click', function(){
-//     alert("Hello  I am Updated One..");
-// })
-
-// let btn2 = document.getElementById('btn2');
-
-// btn2.onclick = function(){
-//     alert("Hello  I am Updated Two..");
-// }
-
-// btn2.onclick = ()=>{
-//     alert("Hello  I am Updated Two..");
-// }
-
-btn1. onclick = function(){
-    alert("taslim khan");
-=======
 // }
 
 // let btn = document.getElementById('btn').addEventListener('click', function(){
@@ -41,5 +17,4 @@ btn1. onclick = function(){
 
 btn2.onclick = ()=>{
     console.log("Hello  I am Updated Two..");
->>>>>>> ed74bd013a06108f46fae9aa20f3bd1c4517e65a
 }
