eccf3055b2e0bebf6458edb69f8df1c95534f7cb
Tasli Khan <tasm8676@gmail.com>
2025-01-10 18:03:00 +0530

    work-done

---
+++ taslim-space/practice/array-pratice/js app.js
@@ -0,0 +1,24 @@
+// let advancearray = [["taslim", "janab", "pranjal" ,"ameen"],[1,2,3,4,5,],["apple","banana","graps"]];
+// let newarray = advancearray.flat();
+// console.log(advancearray );
+
+
+// let array1 = [3,4,6,7,8,5];
+// let array =array1.flat();
+// console.log(array1);
+
+
+// let array2 =["jeeshan","ameen","taslim","pranjal","muskan",];
+// array2.sort();
+// console.log(array2);
+
+// let array3 =["jeeshan","ameen","taslim","pranjal","muskan",];
+// let aray=array3.shift();
+// console.log();
+
+
+let array4 =["apple","banana","grapes","mango","orange"];
+let newarray= array4.slice(0,2);
+console.log(array4);
+
+
