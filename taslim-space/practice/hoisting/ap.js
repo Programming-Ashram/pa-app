8dd3e6b119093095911e9c367a6751285bdcd041
Tasli Khan <tasm8676@gmail.com>
2025-01-15 07:27:38 +0530

    ok

---
+++ taslim-space/practice/hoisting/ap.js
@@ -0,0 +1,31 @@
+"use strict";// useStrict
+
+// let y = 20;
+// console.log(y);
+
+// Hoisting (Use before declear or intialization)
+// x = 10;
+// console.log(x);
+// var x;
+
+// class add,
+let head = document.getElementById('janab');
+let btn = document.getElementById('btn')
+let btnRemove= document.getElementById('btnRemove')
+btn.addEventListener('click', ()=>{
+    head.classList.add('active');  
+})
+btnRemove.addEventListener('click', ()=>{
+    head.classList.remove('active');
+})
+// head.classList.add('active');
+// head.classList.remove('test');
+// head.classList.replace('heading', "newClass");
+
+// class remove,
+// class replace,
+
+
+
+// let  y = 50;
+// console.log(y)
