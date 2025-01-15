77c999b841b2e4117b0204a6101567e12c916b29
pranjal mankar <pranjalmankr33@gmail.com>
2025-01-15 08:54:28 +0530

    kk

---
+++ pranjal-space/practice/javascript/practice-6/javascript/app.js
@@ -0,0 +1,5 @@
+let btn = document.getElementById('btn');
+let name = document.getElementById('name');
+let list = document.getElementById('list');
+let alertWarn = document.getElementById('alertwarn');
+let alertDelete = document.getElementById('alertdelete');
