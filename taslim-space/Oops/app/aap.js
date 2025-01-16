5445300c5fbb2d7df155725b25aa853b276facb9
Tasli Khan <tasm8676@gmail.com>
2025-01-16 07:50:06 +0530

    done

---
+++ taslim-space/Oops/app/aap.js
@@ -0,0 +1,8 @@
+class Car{
+    constructor( carName,carPrice, carColor){
+        this.carName = carName;
+        this.carPrice= carPrice;
+        this.carColor=carColor;
+    }
+    updateColor = (newColor)
+}
