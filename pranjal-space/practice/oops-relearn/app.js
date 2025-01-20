ff469a840e7b5e276e3f542b2d928b0300f6d4e8
pranjal mankar <pranjalmankr33@gmail.com>
2025-01-20 14:26:15 +0530

    hlo

---
+++ pranjal-space/practice/oops-relearn/app.js
@@ -0,0 +1,16 @@
+class Waffers{
+    constructor(waferCategorgy,wafferName){
+        this.stName = stName;
+        this.stClass = stClass;
+    };
+    updateClass = (newClass) =>{
+        this.stClass = newClass;
+
+    }
+
+}
+let st1 =  new  student("pranjal","12th");
+let st2 =  new  student("rina","12th");
+let st3 =  new  student("prakash","12th");
+
+let classInput   
