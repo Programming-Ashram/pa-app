2bc485cbd70c15b8613b26b9be7e286fd542d6d6
AMEEN <afkk86@gmail.com>
2025-01-23 08:53:39 +0530

    Class obj

--- ameen-space/advance-javascript/ES6-echma script/app.js
+++ ameen-space/advance-javascript/ES6-echma script/app.js
@@ -22,4 +22,49 @@
 // let cur = (a) => (b) => (c) => a + b + c;
 // console.log(cur(25)(49)(87));
 
-//
+// let testcur = (a, b) => a + b;
+// console.log(testcur(55)(48));
+
+//Spread
+// let num1 = [1, 2, 3, 4,];
+// let num2 = [...num1, 5, 6, 7, 8];
+// console.log(num2);
+
+//Disturcting
+//  const arr = ["seb", "kela", "angur"];
+//  const obj = {
+//     fname: "Ameen",
+//     age : 25,
+//     add : "Waraseoni"
+//  }
+//  const {fname, age, add, country = "Bharat"} = obj;
+//  console.log(fname, add, age, country);
+
+// //class obj
+// class Student{
+//     constructor(fname,lname){
+//         this.fname = fname;
+//         this.lname = lname;
+//     }
+//     updateLname = (newname) =>{
+//         this.lname = newname;
+//     }
+        
+// }
+
+// Student.prototype.fullname = function(){
+//     return this.fname +"  "+ this.lname;
+// }
+
+// let st1 = new Student("Vaasu", "bhardwaj");
+// st1.updateLname("Ammu");
+// console.log(st1.fullname())
+
+//Try & Catch
+
+ 
+
+
+
+
+
