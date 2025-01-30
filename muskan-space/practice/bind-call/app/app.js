8a42b9dd736f47eb5183b0df05a7ca7dfaa9dabe
muskansh <muskansharnagat@gmail.com>
2025-01-30 08:57:03 +0530

    aa

--- muskan-space/practice/bind-call/app/app.js
+++ muskan-space/practice/bind-call/app/app.js
@@ -54,3 +54,72 @@ let user3 ={
 
 //console.log(getData);
 //console.log(user1.fullName.call(user1));
+
+
+
+
+
+
+//clouser
+//global scope
+let x =10;
+function calculator(){
+    //local scope
+    //let x = 10;
+    function sub(){
+        console.log(x-6);
+
+        function add(){
+            console.log(x + 6);
+        }
+        add();
+
+    }sub();
+}calculator()
+
+function siblingfn(){
+    console.log(x);
+}
+siblingfn();
+
+//call back function
+function template(getData){
+    document.writeln("hello my name is "+ getData);
+}
+
+//---------------[Ayushman]-[chaurasiya]--------------(template(as a function))
+function work (fname, lname, myCallback){
+    //---fetch Data
+    let fetchData  = `${fname}  ${lname}`;
+    // template()
+    myCallback(fetchData);
+}
+
+
+//-----------------------(Function as arg)
+work("Ayushman", "Chaurasiya", template);
+
+function testResult(prpos){
+    console.log(prpos);
+}
+
+
+function apiDataStatus(props){
+    console.log(propos);
+}
+
+let myGreet = new Promise(function(myResolve,myReject){
+    let fname;
+
+    if(fname === "pranjal"){
+        myResolve("found pranjal");
+    }
+
+    else{
+        myReject("Error");
+    }
+});
+myGreet.then(
+    function(value){apiDataStatus(value)},
+    function(error){apiDataStatus(error)}
+)
