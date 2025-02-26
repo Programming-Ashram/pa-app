510e57471d45cf5c686b7bab7a5e0c77b551cdc1
programmingashram <avenuedesign7658@gmail.com>
2025-02-26 09:05:07 +0530

    ssss

--- nidhi-work/loop/app.js
+++ nidhi-work/loop/app.js
@@ -1,11 +1,22 @@
 $(document).ready(function(){
-   $('#btn').click(function(){
-        $('p').slideUp();
-   })
-   $('#show').click(function(){
-    $('p').slideDown();
+    // $('span').parentsUntil('body').css({
+    //     'background-color': 'red'
+    // })
+    // $('.parent').find('span').css(
+    //     {
+    //         'background-color':'green',
+    //     }
+    // )
+    // $('.mega-child-parent h1').nextUntil('h4').css(
+    //     {
+    //         'background-color':'green',
+    //     }
+    // )
+    var buttonText = "Dynamic Button With Var"
+    $('button').append(buttonText);
+    $('button').click(function(){
+      
+        $('h3').filter('.head').toggleClass('active')
     })
-    $('#toggle').click(function(){
-        $('p').slideToggle();
-    })
+
 })
