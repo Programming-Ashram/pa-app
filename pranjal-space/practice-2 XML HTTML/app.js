b4fb15eb3b8c009888999d16c5f30e833d8c2a38
pranjal mankar <pranjalmankr33@gmail.com>
2025-03-04 09:49:56 +0530

    pm

--- pranjal-space/practice-2 XML HTTML/app.js
+++ pranjal-space/practice-2 XML HTTML/app.js
@@ -2,28 +2,23 @@ function fetchmyData(){
     let api = new XMLHttpRequest();
     api.onload = function(){
         let convertData = JSON.parse(this.responseText);
-
         let data = document.getElementById('data');
 
         for(let i in convertData){
             data.innerHTML +=`
                 <div class='col-md-3'>
-                    <div class='card my-2 ${(convertData[i].completed == true) ? "bg-success" : 'bg-warning'}'>
+                    <div class='card my-2 ${(convertData[i].completed == true) ? "bg-success-subtle" : 'bg-warning-subtle'}'>
                         <div class='card-body'>
-                            <div class='d-flex align-items-center justify-content-between text-white'>
+                            <div class='d-flex align-items-center justify-content-between'>
                                 <h3>${convertData[i].id}</h3>
-                                <i class="fa-solid  ${(convertData[i].completed == true) ? "fa-circle-check" : 'fa-hourglass-half'} "></i>
+                                <i class="fa-solid ${(convertData[i].completed == true)? "fa-cicle-check" :'fa-hourglass-half'}"></i>
                             </div>
-                            <p>${convertData[i].title}</p>
                         </div>
                     </div>
                 </div>
             `;
         }
     }
-    api.open("GET", "https://jsonplaceholder.typicode.com/todos");
+    api.open("GET", "");
     api.send();
-
-    
-   
 }fetchmyData()
