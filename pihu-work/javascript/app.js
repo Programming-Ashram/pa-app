22fc78ef5804aaf431439f0dcaeff2736b042b7c
programmingashram <avenuedesign7658@gmail.com>
2025-03-31 18:52:31 +0530

    rr

---
+++ pihu-work/javascript/app.js
@@ -0,0 +1,27 @@
+// alert("Hello");
+// console.log("Hello");
+// document.write("Hello");
+// confirm("Are you want to delete this...?");
+// prompt("Please Enter your name...");
+// console.error("This is test error...");
+
+// Variable
+// var (keyword)
+// var name = "Pihu";
+// var name = "Muskan";
+// console.log(name);
+// let (keyword)
+// let name = "Pihu";
+// let name = "Muskan";
+// console.log(name);
+// const (keyword)
+// const num = 500;
+// console.log(num);
+
+
+let result1 = 50;
+let result2 = 50;
+
+let finalResult = result1 + result2;
+
+console.log(finalResult);
