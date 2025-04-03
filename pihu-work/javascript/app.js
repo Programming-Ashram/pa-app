459ea4426a5c90608ec2ea4d76431c08d62339c0
programmingashram <avenuedesign7658@gmail.com>
2025-04-03 18:44:51 +0530

    xzx

--- pihu-work/javascript/app.js
+++ pihu-work/javascript/app.js
@@ -37,31 +37,49 @@
 // }
 
 // Data Types
-// 1. Premitive
-      // a. String,
-      let name = "Muskan";
-      console.log(typeof(name));
-      // b. Number,
-      let num = 40;
-      console.log(typeof(num));
-      // c. Undefined,
-      let name1;
-      console.log(typeof(name1))
-      // d. Boolean,
-      let bulb = false;
-      console.log(typeof(bulb))
-      // e. Null
-      let salary = null;
-      console.log(typeof(salary));
+// // 1. Premitive
+//       // a. String,
+//       let name = "Muskan";
+//       console.log(typeof(name));
+//       // b. Number,
+//       let num = 40;
+//       console.log(typeof(num));
+//       // c. Undefined,
+//       let name1;
+//       console.log(typeof(name1))
+//       // d. Boolean,
+//       let bulb = false;
+//       console.log(typeof(bulb))
+//       // e. Null
+//       let salary = null;
+//       console.log(typeof(salary));
 
-// 2. Non Premotive
-      // a. Array,
-      let fruits = ["Apple", "Banana", "Mango", "Orange"];
-      console.log(typeof(fruits));
-      // b. Object,
-      let person = {
-        name: "Muskan",
-        age: 23,
-        city: "Delhi"
-      }
-      console.log(typeof(person));
+// // 2. Non Premotive
+//       // a. Array,
+//       let fruits = ["Apple", "Banana", "Mango", "Orange"];
+//       console.log(typeof(fruits));
+//       // b. Object,
+//       let person = {
+//         name: "Muskan",
+//         age: 23,
+//         city: "Delhi"
+//       }
+//       console.log(typeof(person));
+
+// DOM
+
+let nameid = document.getElementById('name');
+console.log(nameid.innerHTML);
+let nameclass = document.getElementsByClassName('name')[2];
+console.log(nameclass.innerText)
+let nameTag = document.getElementsByTagName('h2')[1];
+console.log(nameTag.innerText)
+let nameQuery = document.querySelector('.name');
+let namequeryAll = document.querySelectorAll('.name')[0];
+console.log(namequeryAll);
+
+let newName = document.getElementById('newName');
+
+newName.innerText += " Himanshi";
+newName.style.color = "black";
+newName.style.backgroundColor = "Yellow";
